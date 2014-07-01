685363ef782f040ec1ddada98643fe898ceb0eac
isaacrdz <zack_cpp@hotmail.com>
2014-07-01 17:52:40 -0500

    $ and transition historia

--- codiguito.js
+++ codiguito.js
@@ -1,5 +1,18 @@
 $(document).on("ready",inicio);
 function inicio()
+{	
+	//Aqui va todo lo relacionado con el DOM
+	$("#personalizar").on("click", movida);
+}
+
+function movida()
 {
-	alert("prueba");
+	//JSON
+	var cambiosCSS = 
+	{
+		background:"red",
+		color:"#222"
+	};
+
+	$("#historia").css(cambiosCSS);
 }
