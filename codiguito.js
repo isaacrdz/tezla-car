fd835df45776909daa86813b1f8e85a45ff03398
isaacrdz <zack_cpp@hotmail.com>
2014-07-03 12:00:21 -0500

    finish first part

--- codiguito.js
+++ codiguito.js
@@ -16,6 +16,22 @@ function movida()
 		width:0
 
 	};
+	var cambiosPersonalizacion =
+	{
+		height: "auto",
+		opacity: 1,
+		width: "30%"
+	}
 
 	$("#historia").css(cambiosCSS);
+	$("#personalizacion").css(cambiosPersonalizacion);
+	$("#color div").on("click",cambiarColor);
+}
+
+function cambiarColor(datos)
+{
+	//console.log(datos); saber lo de abajo en la consola al dar click en el div de color 
+	var colorito = datos.currentTarget.id;
+	var nuevoCoche = "c" + colorito +".jpg";
+	$("#cochecito img").attr("src",nuevoCoche);
 }
